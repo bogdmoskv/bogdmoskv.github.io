f6bdb32174f3de3921821b98f926c2b0943c5dc1
“Bogdan” <bogd.moskv@gmail.com>
2024-11-12 21:17:28 +0200

    commit

---
+++ scripts/contact-page.js
@@ -0,0 +1,8 @@
+const map = L.map('map').setView([50.451646256017916, 30.412360596056462], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);
+
+L.marker([50.451646256017916, 30.412360596056462]).addTo(map)
+    .bindPopup('AS-Trans<br>Київ вул. Радищева, 3 офіс Б-306, а/с 25');
