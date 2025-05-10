6c012d91561c314a0e4624ff44519f4504a543dd
“Bogdan” <bogd.moskv@gmail.com>
2025-05-10 12:46:01 +0300

    commit

---
+++ scripts/main-page.js
@@ -0,0 +1,6 @@
+let language = localStorage.getItem('selectedLanguage')
+if (language == "ukrainian"){
+    document.title = "Найкраща міжнародна логістична компанія AS-Trans Київ, Україна"
+} else if (language == "american"){
+    document.title = "International Logistic transportation company - AS-Trans website"
+}
